cbb46ef9f60a3e0b7d24e4a8b1990b70763df36f
Mahbubul Alam <evan@hostrare.com>
2022-08-23 08:14:36 +0600

    db update calll 45

--- database/laravelproject.sql
+++ database/laravelproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 06, 2022 at 04:25 AM
+-- Generation Time: Aug 23, 2022 at 04:02 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.13
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `laravelproject`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `status` tinyint(1) DEFAULT 1,
+  `creator` int(11) DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `status`, `creator`, `created_at`, `updated_at`) VALUES
+(1, 'Honey', 1, 1, '2022-08-22 19:54:09', '2022-08-23 01:54:09');
 
 -- --------------------------------------------------------
 
@@ -79,7 +101,8 @@ CREATE TABLE `password_resets` (
 --
 
 INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
-('evan@hostrare.com', '$2y$10$8M2McJf3QM.IjnucehIsuOCGiSEj6Gs4P4LE/goJ.6ex9KGZfgXpq', '2022-07-05 10:24:13');
+('evan@hostrare.com', '$2y$10$8M2McJf3QM.IjnucehIsuOCGiSEj6Gs4P4LE/goJ.6ex9KGZfgXpq', '2022-07-05 10:24:13'),
+('evanpab@gmail.com', '$2y$10$SgWnO5iA.9fOrV5UBj9npu3pp.8PNuRASS19XcWhVZCpbMZrPODji', '2022-08-15 20:27:18');
 
 -- --------------------------------------------------------
 
@@ -109,8 +132,10 @@ CREATE TABLE `posts` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `section` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `picture` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `faicon` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `creator` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -119,18 +144,42 @@ CREATE TABLE `posts` (
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`id`, `section`, `picture`, `title`, `content`, `created_at`, `updated_at`) VALUES
-(11, 'services', NULL, 'E-Commerce', '<b>Lorem ipsum</b> dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.', '2022-08-04 19:57:37', '2022-08-05 19:36:58'),
-(12, 'services', NULL, 'Responsive Design', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.', '2022-08-04 19:57:53', '2022-08-04 19:57:53'),
-(13, 'services', NULL, 'Web Security', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.', '2022-08-04 19:58:06', '2022-08-04 19:58:06'),
-(14, 'portfolio', '1659751913.jpg', 'Threads', 'Illustration', '2022-08-05 19:37:24', '2022-08-05 20:11:53'),
-(15, 'portfolio', '1659751609.jpg', 'Explore', 'Graphic Design', '2022-08-05 19:37:39', '2022-08-05 20:06:49'),
-(16, 'portfolio', '1659751618.jpg', 'Finish', 'Identity', '2022-08-05 19:37:54', '2022-08-05 20:06:58'),
-(17, 'about', '1659752062.jpg', '2009-2011 <br>Our Humble Beginnings', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!', '2022-08-05 19:43:34', '2022-08-05 20:14:22'),
-(18, 'about', '1659752070.jpg', 'March 2011 <br> An Agency is Born', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!', '2022-08-05 19:43:53', '2022-08-05 20:14:30'),
-(19, 'team', '1659752119.jpg', 'Md Omar Faruck', 'Developer', '2022-08-05 19:50:11', '2022-08-05 20:15:19'),
-(20, 'team', '1659752126.jpg', 'Opu Saha', 'Developer', '2022-08-05 19:50:32', '2022-08-05 20:15:26'),
-(21, 'team', '1659752133.jpg', 'Durjoy', 'Developer', '2022-08-05 19:50:46', '2022-08-05 20:15:33');
+INSERT INTO `posts` (`id`, `section`, `picture`, `faicon`, `title`, `content`, `creator`, `created_at`, `updated_at`) VALUES
+(11, 'services', '1660613316.png', 'fa-shopping-cart', 'E-Commerce', '<b>Lorem ipsum</b> dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.', 1, '2022-08-04 19:57:37', '2022-08-19 19:43:02'),
+(12, 'services', '1660613330.png', 'fa-laptop', 'Responsive Design', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.', 1, '2022-08-04 19:57:53', '2022-08-19 19:44:14'),
+(13, 'services', '1660613340.png', 'fa-lock', 'Web Security', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.', 1, '2022-08-04 19:58:06', '2022-08-19 19:44:31'),
+(14, 'portfolio', '1659751913.jpg', NULL, 'Threads', 'Illustration', 1, '2022-08-05 19:37:24', '2022-08-05 20:11:53'),
+(15, 'portfolio', '1659751609.jpg', NULL, 'Explore', 'Graphic Design', 1, '2022-08-05 19:37:39', '2022-08-05 20:06:49'),
+(16, 'portfolio', '1659751618.jpg', NULL, 'Finish', 'Identity', 1, '2022-08-05 19:37:54', '2022-08-05 20:06:58'),
+(17, 'about', '1659752062.jpg', NULL, '2009-2011 <br>Our Humble Beginnings', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!', 1, '2022-08-05 19:43:34', '2022-08-05 20:14:22'),
+(18, 'about', '1659752070.jpg', NULL, 'March 2011 <br> An Agency is Born', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!', 1, '2022-08-05 19:43:53', '2022-08-05 20:14:30'),
+(19, 'team', '1659752119.jpg', NULL, 'Md Omar Faruck', 'Developer', 1, '2022-08-05 19:50:11', '2022-08-05 20:15:19'),
+(22, 'about', '1660614059.jpg', NULL, 'December 2015 Transition to Full Service', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!', 1, '2022-08-15 19:40:59', '2022-08-15 19:40:59'),
+(23, 'about', '1660614114.jpg', NULL, 'July 2020 Phase Two Expansion', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!', 1, '2022-08-15 19:41:54', '2022-08-15 19:41:54'),
+(24, 'brand', '1660617359.svg', NULL, 'Microsoft', 'Microsoft', 1, '2022-08-15 20:35:59', '2022-08-15 20:35:59'),
+(25, 'team', NULL, NULL, 'Test post', 'test', 3, '2022-08-16 19:27:02', '2022-08-16 19:27:02'),
+(26, 'team', NULL, NULL, 'Test post 2', 'test', 3, '2022-08-16 19:29:08', '2022-08-16 19:29:08'),
+(27, 'team', NULL, NULL, 'Test 3', 'Test post 3', 1, '2022-08-16 19:34:10', '2022-08-16 19:34:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  `catid` int(11) NOT NULL,
+  `picture` varchar(200) DEFAULT NULL,
+  `summary` text NOT NULL,
+  `details` longtext DEFAULT NULL,
+  `price` varchar(20) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
+  `creator` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -166,7 +215,10 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('HT0nbw2Z4wDbiZEOtiODTDZg06hj9sBVqXltbkAo', 1, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiRjcyVlo1aEh5OVdETTFMUzdZbmlpNTQyT21rSzVhMWpaSDdkblFENSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly9sb2NhbGhvc3QvbGFyYXZlbHdlYnNpdGUvcG9zdHMiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6MjE6InBhc3N3b3JkX2hhc2hfc2FuY3R1bSI7czo2MDoiJDJ5JDEwJGZjcUhPY3F4WlpqTFE0S215c1p4WHVCYk1LT0lkQTAwbDk2MHJwM2EzRGJZRnB6Z2ZZWUhDIjtzOjc6Im1lc3NhZ2UiO3M6MTY6IlNhdmUgU3VjY2Vzc2Z1bCEiO30=', 1659752619);
+('CE0YepYXmneLUHKdOLpBTX7BsvZlysySHi59QU7Z', 1, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiYXZGTU1lMHJrMnFhc1JHR2VpVlc0VzJsWUQ2QnVvVW51Mmo2OGJVViI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly9sb2NhbGhvc3QvbGFyYXZlbHdlYnNpdGUiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6MjE6InBhc3N3b3JkX2hhc2hfc2FuY3R1bSI7czo2MDoiJDJ5JDEwJGZjcUhPY3F4WlpqTFE0S215c1p4WHVCYk1LT0lkQTAwbDk2MHJwM2EzRGJZRnB6Z2ZZWUhDIjtzOjc6Im1lc3NhZ2UiO3M6MTY6IlNhdmUgU3VjY2Vzc2Z1bCEiO30=', 1661220046),
+('L8kJUOv3x26eQ2TcAHTJUJM6nQsfA0OPbDHznyfX', 1, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiRkR6eW1kRENUTm5jRmI5ZVlEWnVlRFplTHd6Ylh4NnFFZkdZTkhOaiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly9sb2NhbGhvc3QvbGFyYXZlbHdlYnNpdGUiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6MjE6InBhc3N3b3JkX2hhc2hfc2FuY3R1bSI7czo2MDoiJDJ5JDEwJGZjcUhPY3F4WlpqTFE0S215c1p4WHVCYk1LT0lkQTAwbDk2MHJwM2EzRGJZRnB6Z2ZZWUhDIjt9', 1660789300),
+('nLBKCIUAMCfjtbWdqEibg3I4QngBrYV8Ywz6VBGz', 1, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiZHVPMFMxOWhlNWZjM2prMWkxdUVqcXozcVJUc2VNdmtza3R6RmZkQyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDE6Imh0dHA6Ly9sb2NhbGhvc3QvbGFyYXZlbHdlYnNpdGUvZGFzaGJvYXJkIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTtzOjIxOiJwYXNzd29yZF9oYXNoX3NhbmN0dW0iO3M6NjA6IiQyeSQxMCRmY3FIT2NxeFpaakxRNEtteXNaeFh1QmJNS09JZEEwMGw5NjBycDNhM0RiWUZwemdmWVlIQyI7czo3OiJtZXNzYWdlIjtzOjE2OiJTYXZlIFN1Y2Nlc3NmdWwhIjt9', 1660961291),
+('wTJSL9oX8VzyIamLa3E3F8tL2lYQqfA7IXqP362W', NULL, '127.0.0.1', 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiOGZNOVYzSjJ5aDdnSk9uSWdQOFljNDZiMUpqOGNWVkFzZ0dOdmxNciI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDQ6Imh0dHA6Ly9sb2NhbGhvc3QvbGFyYXZlbHdlYnNpdGUvcG9zdFJlYXJhbmdlIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1660789141);
 
 -- --------------------------------------------------------
 
@@ -196,11 +248,18 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `two_factor_confirmed_at`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
 (1, 'Md Mahbubul Alam', 'evanpab@gmail.com', NULL, '$2y$10$fcqHOcqxZZjLQ4KmysZxXuBbMKOIdA00l960rp3a3DbYFpzgfYYHC', NULL, NULL, NULL, NULL, NULL, NULL, '2022-06-04 10:22:17', '2022-06-04 10:22:17'),
-(2, 'Evan', 'evan@hostrare.com', NULL, '$2y$10$5zpbW7zxM/MqvlBRBtFvFOYx98oF.UGIwbXbAF5TDXf5EzdX3wGrW', NULL, NULL, NULL, 'uVn0d7lck0hD63mAHubYZcsHjABQlJr1Pdn5XeOy2FTVstK0jKAIORnMzDK1', NULL, NULL, '2022-06-07 10:28:25', '2022-07-05 10:06:17');
+(2, 'Evan', 'evan@hostrare.com', NULL, '$2y$10$5zpbW7zxM/MqvlBRBtFvFOYx98oF.UGIwbXbAF5TDXf5EzdX3wGrW', NULL, NULL, NULL, 'uVn0d7lck0hD63mAHubYZcsHjABQlJr1Pdn5XeOy2FTVstK0jKAIORnMzDK1', NULL, NULL, '2022-06-07 10:28:25', '2022-07-05 10:06:17'),
+(3, 'Evan BIndin', 'evan@bindim.com', NULL, '$2y$10$Oj/tkGSyq062O7Lboi1KIOhSxZX3kBnTKwN2NUhHF7KOmOFL9HMSW', NULL, NULL, NULL, NULL, NULL, NULL, '2022-08-15 20:18:21', '2022-08-15 20:18:21');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -236,6 +295,12 @@ ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `roles`
 --
 ALTER TABLE `roles`
@@ -261,6 +326,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -282,7 +353,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -294,7 +371,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
