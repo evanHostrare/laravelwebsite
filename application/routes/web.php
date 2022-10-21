5a20f189d153712fa06a3f305712565631d985a5
Mahbubul Alam <evan@hostrare.com>
2022-10-21 08:58:10 +0600

    vue install

--- application/routes/web.php
+++ application/routes/web.php
@@ -76,3 +76,7 @@ Route::get('/dashboard', function () {
 // ])->group(function () {
     
 // });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
